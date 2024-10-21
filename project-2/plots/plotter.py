93c21c7e234ef9ed4a661dea9b49a259821e298b
Adrian Borup <adrian@borup.com>
2024-10-21 14:34:11 +0200

    Add latency plot for each query

--- project-2/plots/plotter.py
+++ project-2/plots/plotter.py
@@ -7,14 +7,22 @@ import sys
 import pathlib
 
 
+work_dir = pathlib.Path(__file__).parent.resolve()
+
+
 def make_out_path(name, format):
-    work_dir = pathlib.Path(__file__).parent.resolve()
     out_dir = work_dir / "output" / format
     return out_dir / f"{name}.{format}"
 
 
 def save_plot(name):
     format = "pdf"
+
+    if not (work_dir / "output").exists():
+        (work_dir / "output").mkdir()
+    if not (work_dir / "output" / format).exists():
+        (work_dir / "output" / format).mkdir()
+
     plt.savefig(make_out_path(name, format),
                 format=format, bbox_inches="tight")
 
@@ -66,9 +74,56 @@ class Measurement:
 class Configuration:
     key: str
     measurements: list[Measurement]
+    query: str = ""
+    threads: int = -1
+    scaling_factor: int = -1
+
+    def __post_init__(self):
+        self.query = self.measurements[0].query
+        self.threads = int(self.measurements[0].threads)
+        self.scaling_factor = int(self.measurements[0].scaling_factor)
 
     def average_by(self, key: str):
         return sum([getattr(m, key) for m in self.measurements]) / len(self.measurements)
+
+
+def get_queries(configs: list[Configuration]):
+    return sorted(set(c.query for c in configs))
+
+
+def get_threads(configs: list[Configuration]):
+    return sorted(set(c.threads for c in configs))
+
+
+def get_scaling_factors(configs: list[Configuration]):
+    return sorted(set(c.scaling_factor for c in configs))
+
+
+def plot_latency(configs: list[Configuration]):
+    # y_max = max(max(m.elapsed_time for m in c.measurements) for c in configs)
+
+    for q in get_queries(configs):
+        plt.figure()
+        ax = plt.gca()
+
+        for i, t in enumerate(get_threads(configs)):
+            cs = [c for c in configs if c.query == q and c.threads == t]
+            cs.sort(key=lambda x: x.scaling_factor)
+
+            xs = [c.scaling_factor for c in cs]
+            ys = [c.average_by("elapsed_time") for c in cs]
+
+            marker, facecolor = MARKERS[i]
+            plt.loglog(xs, ys, f'-{marker}',
+                       markerfacecolor=facecolor, label=f"{t} threads")
+
+        plt.title(f"Query {q} latency")
+
+        plt.legend()
+        plt.ylabel("Query Latency (s)")
+        plt.xlabel("Scaling Factor")
+
+        save_plot("latency-q" + q)
 
 
 def read_data(file):
@@ -87,3 +142,5 @@ if __name__ == "__main__":
     configurations = [Configuration(key=k, measurements=list(g)) for k, g in groupby(
         sorted(measurements, key=lambda x: x.configuration_key()), lambda x: x.configuration_key())]
     configurations.sort(key=lambda x: x.key)
+
+    plot_latency(configurations)
